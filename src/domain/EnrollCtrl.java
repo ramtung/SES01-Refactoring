14420ef282b02f515d565c2a12fce540ccb329b3
anary <mehranhosseinnejad81@gmail.com>
2023-01-10 22:26:36 +0330

    change cse class name to Offering

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import domain.exceptions.EnrollmentRulesViolationException;
 
 public class EnrollCtrl {
-    public void enroll(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
+    public void enroll(Student student, List<Offering> courses) throws EnrollmentRulesViolationException {
         Map<Term, Map<Course, Double>> transcript = student.getTranscript();
-        for (CSE o : courses) {
+        for (Offering o : courses) {
             for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
                 for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
                     if (r.getKey().equals(o.getCourse()) && r.getValue() >= 10)
@@ -26,7 +26,7 @@ public class EnrollCtrl {
                 }
                 throw new EnrollmentRulesViolationException(String.format("The student has not passed %student as a prerequisite of %student", pre.getName(), o.getCourse().getName()));
             }
-            for (CSE o2 : courses) {
+            for (Offering o2 : courses) {
                 if (o == o2)
                     continue;
                 if (o.getExamTime().equals(o2.getExamTime()))
@@ -36,7 +36,7 @@ public class EnrollCtrl {
             }
         }
         int unitsRequested = 0;
-        for (CSE o : courses)
+        for (Offering o : courses)
             unitsRequested += o.getCourse().getUnits();
         double points = 0;
         int totalUnits = 0;
@@ -51,7 +51,7 @@ public class EnrollCtrl {
                 (gpa < 16 && unitsRequested > 16) ||
                 (unitsRequested > 20))
             throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
-        for (CSE o : courses)
+        for (Offering o : courses)
             student.takeCourse(o.getCourse(), o.getSection());
     }
 }
